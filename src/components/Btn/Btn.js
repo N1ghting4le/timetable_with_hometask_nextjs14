e7951ef134b7c170fc6c4bd68b1ed7a27e878d15
Nikita <112396040+N1ghting4le@users.noreply.github.com>
2024-07-22 12:05:08 +0300

    Delete src/components/Btn directory

--- src/components/Btn/Btn.js
+++
@@ -1,7 +0,0 @@
-import styles from "./btn.module.css";
-
-const Btn = ({ onClick, children, className }) => (
-    <button className={className || styles.button} onClick={onClick}>{children}</button>
-);
-
-export default Btn;
