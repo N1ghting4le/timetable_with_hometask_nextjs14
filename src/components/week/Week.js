b0fb45d3383c73838f8717514a125141ffe6b65d
N1ghting4le <nikansoloveib@gmail.com>
2024-08-02 21:10:16 +0300

    version 2.0.0 week sliding animation is no longer determined by state

--- src/components/week/Week.js
+++ src/components/week/Week.js
@@ -9,23 +9,26 @@ const Week = ({ weekIndex }) => {
     const { curr, prevCurr } = useCurr();
     const { days } = useWeek(weekIndex);
     const [display, setDisplay] = useState(curr === weekIndex);
-    const [animation, setAnimation] = useState(null);
     const ref = useRef({});
 
     useEffect(() => {
         if (curr === weekIndex) {
             setDisplay(true);
-            setAnimation(() => {
-                if (prevCurr === weekIndex) return null;
-                return styles[prevCurr < weekIndex ? 'fromRight' : 'fromLeft'];
-            });
-        } else if (display) {
+        } else if (prevCurr === weekIndex) {
             setTimeout(() => setDisplay(false), 300);
             
             ref.current.style.position = 'absolute';
             ref.current.style.transform = `translateX(${curr > weekIndex ? -100 : 100}%)`;
+        } else if (display) {
+            setDisplay(false);
         }
     }, [curr]);
+
+    const chooseAnimation = () => {
+        if (prevCurr === weekIndex) return '';
+
+        return styles[prevCurr < weekIndex ? 'fromRight' : 'fromLeft'];
+    }
 
     const renderDays = () => days.map((item, i) => (
         <Day key={item.date}
@@ -36,7 +39,7 @@ const Week = ({ weekIndex }) => {
     const elements = renderDays();
 
     return display ? (
-        <div className={`${styles.week} ${animation}`} ref={el => ref.current = el}>
+        <div className={`${styles.week} ${chooseAnimation()}`} ref={el => ref.current = el}>
             {elements}
         </div>
     ) : null;
