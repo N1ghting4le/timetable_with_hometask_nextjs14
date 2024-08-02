05d2a0d380a622df6ed0b353254ad45506f40e68
N1ghting4le <nikansoloveib@gmail.com>
2024-08-02 19:36:22 +0300

    version 2.0.0 border radius of day item on mobile is now the same for every edge

--- src/components/week/Week.js
+++ src/components/week/Week.js
@@ -9,19 +9,19 @@ const Week = ({ weekIndex }) => {
     const { curr, prevCurr } = useCurr();
     const { days } = useWeek(weekIndex);
     const [display, setDisplay] = useState(curr === weekIndex);
-    const [side, setSide] = useState(null);
+    const [animation, setAnimation] = useState(null);
     const ref = useRef({});
 
     useEffect(() => {
         if (curr === weekIndex) {
             setDisplay(true);
-            setSide(() => {
+            setAnimation(() => {
                 if (prevCurr === weekIndex) return null;
                 return styles[prevCurr < weekIndex ? 'fromRight' : 'fromLeft'];
             });
         } else if (display) {
             setTimeout(() => setDisplay(false), 300);
-    
+            
             ref.current.style.position = 'absolute';
             ref.current.style.transform = `translateX(${curr > weekIndex ? -100 : 100}%)`;
         }
@@ -36,7 +36,7 @@ const Week = ({ weekIndex }) => {
     const elements = renderDays();
 
     return display ? (
-        <div className={`${styles.week} ${side}`} ref={el => ref.current = el}>
+        <div className={`${styles.week} ${animation}`} ref={el => ref.current = el}>
             {elements}
         </div>
     ) : null;
