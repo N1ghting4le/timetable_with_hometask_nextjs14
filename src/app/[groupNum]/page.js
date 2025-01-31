f39fe47ef57d19548533f89d7ab64145e4a58033
N1ghting4le <nikansoloveib@gmail.com>
2025-01-31 10:06:42 +0300

    version 3.0.2 metadata update

--- src/app/[groupNum]/page.js
+++ src/app/[groupNum]/page.js
@@ -1,6 +1,12 @@
 import { notFound } from 'next/navigation';
 import WeekList from '@/components/weekList/WeekList';
 
+export async function generateMetadata({ params }) {
+  return {
+    title: 'Расписание группы ' + params.groupNum
+  }
+}
+
 export default function Page({ params }) {
   if (Number.isNaN(+params.groupNum)) return notFound();
 
--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,10 @@
 import GroupsForm from '@/components/groupsForm/GroupsForm';
 
+export const metadata = {
+  title: 'Список групп',
+  description: 'Страница для поиска и выбора группы из списка групп'
+}
+
 export default function Home() {
   return <GroupsForm/>;
 }
